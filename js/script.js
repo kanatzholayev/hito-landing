6bc8c6de7bc2076cb57d8853a6770afb2f08b8dc
kanatzholayev <73548604+kanatzholayev@users.noreply.github.com>
2022-06-22 16:12:55 +0600

    add typing animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-var acc = document.getElementsByClassName("c-accordion__btn");
-var i;
+const acc = document.getElementsByClassName("c-accordion__btn");
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
 	acc[i].addEventListener("click", function () {
 		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
+		const panel = this.nextElementSibling;
 		if (panel.style.maxHeight) {
 			panel.style.maxHeight = null;
 		} else {
@@ -12,3 +11,54 @@ for (i = 0; i < acc.length; i++) {
 		}
 	});
 }
+
+const typedTextSpan = document.querySelector(".c-typed-text");
+const cursorSpan = document.querySelector(".c-cursor");
+
+const textArray = [
+	"a ten-page manual",
+	"a degree in engineering",
+	"a geeky friend",
+	"to trawl the entire web",
+];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains("typing"))
+			cursorSpan.classList.add("typing");
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		cursorSpan.classList.remove("typing");
+		setTimeout(erase, newTextDelay);
+	}
+}
+
+function erase() {
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains("typing"))
+			cursorSpan.classList.add("typing");
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(
+			0,
+			charIndex - 1
+		);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		cursorSpan.classList.remove("typing");
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + 1100);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	// On DOM Load initiate the effect
+	if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
